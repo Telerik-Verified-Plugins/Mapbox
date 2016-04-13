118a064fa9a1081afa3c3dc79294ed7d17be31f5
daniel <daniel.neveux@gmail.com>
2016-04-13 11:29:38 +0200

    fix getAllChildren

--- www/Mapbox.js
+++ www/Mapbox.js
@@ -1,8 +1,118 @@
 var exec = require("cordova/exec");
-  
+
+
+function getAbsoluteMargins(mapDiv) {
+
+  var pageRect = getPageRect();
+
+  var rect = mapDiv.getBoundingClientRect();
+
+  return {
+    'top': pageRect.top + rect.top,
+    'right':  (pageRect.left + pageRect.width) - rect.right,
+    'bottom': (pageRect.top + pageRect.height) - rect.bottom,
+    'left': pageRect.left + rect.left
+  };
+}
+
+
+function getDomElementsOverlay(mapDiv){
+  var children = getAllChildren(mapDiv);
+
+  var elements = [];
+  var element, elemId;
+
+  for (var i = 0; i < children.length; i++) {
+    element = children[i];
+    elemId = element.getAttribute("__pluginDomId");
+    if (!elemId) {
+      elemId = "pmb" + Math.floor(Math.random() * Date.now()) + i;
+      element.setAttribute("__pluginDomId", elemId);
+    }
+    elements.push({
+      id: elemId,
+      size: getDivRect(element)
+    });
+    i++;
+  }
+  return elements;
+}
+
+function getAllChildren(root) {
+  var list = [];
+  var clickable;
+  var style, displayCSS, opacityCSS, visibilityCSS;
+  var search = function(node) {
+    while (node != null) {
+      if (node.nodeType == 1) {
+        style = window.getComputedStyle(node);
+        visibilityCSS = style.getPropertyValue('visibility');
+        displayCSS = style.getPropertyValue('display');
+        opacityCSS = style.getPropertyValue('opacity');
+        if (displayCSS !== "none" && opacityCSS > 0 && visibilityCSS != "hidden") {
+          clickable = node.getAttribute("data-clickable");
+          if (clickable &&
+            clickable.toLowerCase() === "false" &&
+            node.hasChildNodes()) {
+            Array.prototype.push.apply(list, getAllChildren(node));
+          } else {
+            list.push(node);
+          }
+        }
+      }
+      node = node.nextSibling;
+    }
+  };
+  for (var i = 0; i < root.childNodes.length; i++) {
+    search(root.childNodes[i]);
+  }
+  return list;
+}
+
+
+function getDivRect(div) {
+  if (!div) {
+    return;
+  }
+
+  var pageRect = getPageRect();
+
+  var rect = div.getBoundingClientRect();
+  return {
+    'left': rect.left + pageRect.left,
+    'top': rect.top + pageRect.top,
+    'width': rect.width,
+    'height': rect.height
+  };
+}
+
+function getPageRect() {
+  var doc = document.documentElement;
+
+  var pageWidth = window.innerWidth ||
+      document.documentElement.clientWidth ||
+      document.body.clientWidth,
+    pageHeight = window.innerHeight ||
+      document.documentElement.clientHeight ||
+      document.body.clientHeight;
+  var pageLeft = (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0);
+  var pageTop = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+
+  return {
+    'width': pageWidth,
+    'height': pageHeight,
+    'left': pageLeft,
+    'top': pageTop
+  };
+}
+
 module.exports = {
   show: function (options, successCallback, errorCallback, id) {
     id = id || 0;
+    if(options.div){
+      options.HTMLs = getDomElementsOverlay(options.div);
+      options.margins = getAbsoluteMargins(options.div);
+    }
     cordova.exec(successCallback, errorCallback, "Mapbox", "show", [id, options]);
   },
 
